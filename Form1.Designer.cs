a1f3170c844903a6315cf6445e447bae07281172
HoustonCharly <jriverar1801@alumno.ipn.mx>
2023-07-11 14:04:51 -0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Ecuaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tB2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lB1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.equipoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bahnschrift Light", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(221, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Computer Modern", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ecuacion Lineal con una Sola Incogita";
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("Bahnschrift Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb1.Location = new System.Drawing.Point(302, 90);
+            this.tb1.Multiline = true;
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(141, 32);
+            this.tb1.TabIndex = 3;
+            // 
+            // tB2
+            // 
+            this.tB2.Font = new System.Drawing.Font("Bahnschrift Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tB2.Location = new System.Drawing.Point(98, 90);
+            this.tB2.Multiline = true;
+            this.tB2.Name = "tB2";
+            this.tB2.Size = new System.Drawing.Size(141, 32);
+            this.tB2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(251, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "=";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lB1
+            // 
+            this.lB1.Font = new System.Drawing.Font("Bahnschrift Light", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lB1.FormattingEnabled = true;
+            this.lB1.ItemHeight = 21;
+            this.lB1.Items.AddRange(new object[] {
+            " "});
+            this.lB1.Location = new System.Drawing.Point(118, 144);
+            this.lB1.Name = "lB1";
+            this.lB1.Size = new System.Drawing.Size(296, 25);
+            this.lB1.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.equipoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(574, 27);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // equipoToolStripMenuItem
+            // 
+            this.equipoToolStripMenuItem.Font = new System.Drawing.Font("Computer Modern", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equipoToolStripMenuItem.Name = "equipoToolStripMenuItem";
+            this.equipoToolStripMenuItem.Size = new System.Drawing.Size(72, 23);
+            this.equipoToolStripMenuItem.Text = "Equipo";
+            this.equipoToolStripMenuItem.Click += new System.EventHandler(this.equipoToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 276);
+            this.Controls.Add(this.lB1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tB2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Ecuaciones Lineales.";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tB2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lB1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem equipoToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using info.lundin.math;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ecuaciones
+{
+    public partial class Form1 : Form
+    {
+        string xd, xi;//Variables para cuando introduzca datos
+        double xizq, numizq, xder, //Variables para 
+        numder, xfin, nfin, salida;//Operaciones en el calculo de X
+
+        private void equipoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Los Increibles.\n" +
+                            "");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lB1.Items.Clear();//Limpia por si se evalua de nuevo
+            
+            Entrada();
+            
+            numizq = funcion(0, xi);//se hace una evaluacion con x=0 para que solo queden el numero total de x
+            numder = funcion(0, xd);//se hace una evaluacion con x=0 para que solo queden el numero total de x
+            xizq = funcion(1, xi)-numizq;// se evalua x=1 para operar las x con el valor entero lado izq
+            xder = funcion(1, xd)-numder;// se evalua x=1 para operar las x con el valor entero lado der
+            //se opera ya con los numeros de los coeficientes de x como de los numeros solos
+            xfin = xizq - xder;
+            nfin = numder - numizq;
+
+            Salida();
+        }
+        public void Entrada()
+        {
+            //Hace lectura de lo que introduce en el tezbos de la der e izq
+            xd = tb1.Text;
+            xi = tB2.Text;
+        }
+
+        public void Salida()
+        {
+            //imprime lo  que resulta de operar en ambos lados de la ecuacio en una sola variable (salida)
+           salida = nfin / xfin;
+           lB1.Items.Add("El valor de x =  " + salida);
+        }
+
+        public double funcion(double x, string res)
+        {
+            //Se hace la conversion de lo que entra el tezbos de cadena a double para poder operar 
+            //de modo que si existe una x en la ecuacion la sustituye por otra x pero de tipo double
+            ExpressionParser paser1 = new ExpressionParser();
+            paser1.Values.Add("x", x);
+            return (paser1.Parse(res));
+        }
+    }
+}
